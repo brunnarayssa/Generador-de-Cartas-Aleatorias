ebcd937ef3c05e4b32f850b496d0deb76e7f3d74
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2024-12-16 18:12:13 +0000

    subiendoproyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,82 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  createCard();
 };
+
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function paloColor(palo) {
+  return palo === "♥" || palo === "♦" ? "text-danger" : "text-dark";
+}
+
+const card = document.querySelector("#card");
+function createCard() {
+  const cardSymbols = ["♥", "♦", "♣", "♠"];
+  const cardNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const randomPalo = getRandomElement(cardSymbols);
+  const randomNumber = getRandomElement(cardNumbers);
+  const paloColorValue = paloColor(randomPalo);
+  const number = document.querySelector("#number");
+  number.innerHTML = randomNumber;
+  number.className = paloColorValue;
+  const up = document.querySelector("#up");
+  up.innerHTML = randomPalo;
+  up.className = paloColorValue;
+  const bottom = document.querySelector("#bottom");
+  bottom.innerHTML = randomPalo;
+  bottom.className = paloColorValue;
+}
+
+let time = document.querySelector("#time");
+function updateCountdown(i) {
+  time.innerHTML = `La nueva carta se genera en: ${i}`;
+  if (i === 0) {
+    i = 10;
+    createCard();
+  }
+  setTimeout(() => updateCountdown(i - 1), 1000);
+}
+updateCountdown(10);
+
+const button = document.querySelector("#button");
+button.addEventListener("click", () => createCard());
+
+const heightInput = document.getElementById("heightInput");
+heightInput.addEventListener("input", function() {
+  const heightValue = heightInput.value;
+  card.style.height = heightValue ? heightValue + "px" : "";
+  heightInput.addEventListener("blur", function() {
+    if (!heightInput.value) {
+      card.style.height = "";
+    }
+  });
+});
+
+const widthInput = document.getElementById("widthInput");
+widthInput.addEventListener("input", function() {
+  const widthValue = widthInput.value;
+  card.style.width = widthValue ? widthValue + "px" : "";
+  widthInput.addEventListener("blur", function() {
+    if (!widthInput.value) {
+      card.style.width = "";
+    }
+  });
+});
